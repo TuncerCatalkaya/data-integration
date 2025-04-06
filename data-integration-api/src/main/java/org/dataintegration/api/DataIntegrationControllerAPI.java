8414b07572b7616bf5dae64a80e24daf1c53fb77
Tuncer <Tuncercatalkaya@hotmail.com>
2025-04-07 01:16:21 +0200

    WIP

--- data-integration-api/src/main/java/org/dataintegration/api/DataIntegrationControllerAPI.java
+++ data-integration-api/src/main/java/org/dataintegration/api/DataIntegrationControllerAPI.java
@@ -7,37 +7,6 @@ import org.dataintegration.model.DataIntegrationAPIModel;
 import org.dataintegration.model.DataIntegrationHeaderAPIModel;
 import org.dataintegration.model.DataIntegrationInputAPIModel;
 
-/**
- * API for data integration REST controller.
- * Provides which endpoint implementations need to be considered. Default implementations are included.
- * <p>
- * <b>Example implementation for spring:</b>
- * <pre>
- * {@code
- * @RestController
- * @RequestMapping("/example/spring")
- * public class ExampleSpringRestController extends DataIntegrationControllerAPI<String> {
- *
- *     public ExampleSpringRestController(DataIntegrationEndpointsAPI dataIntegrationEndpointsAPI, DataIntegrationAPI<String> dataIntegrationAPI) {
- *         super(dataIntegrationEndpointsAPI, dataIntegrationAPI);
- *     }
- *
- *     @GetMapping
- *     @Override
- *     public DataIntegrationEndpointsAPIModel endpointsRestCall() {
- *         return super.defaultEndpointsRestCall();
- *     }
- *
- *     @PostMapping
- *     @Override
- *     public DataIntegrationAPIModel integrationRestCall(@RequestBody List<String> context) {
- *         return super.defaultIntegrationRestCall(context);
- *     }
- *
- * }
- * }
- * </pre>
- */
 @Getter(AccessLevel.PROTECTED)
 @RequiredArgsConstructor
 public abstract class DataIntegrationControllerAPI {
